ec1b64c5eff5904f940e539a628aa6658af4cda7
@mobolade <@mobolade.akinlua@outlook.com>
2022-04-26 18:56:49 +0100

    build

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-// import "./App.css";
-// import MyForm from "./components/form";
-
-// function App() {
-//   return (
-//     <>
-//       <MyForm />
-//     </>
-//   );
-// }
-
-// export default App;
-
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Form from "./components/form";
 
